e07aa85e1a88f98b34fceb3d6b46fd8c4a071bf9
equeslevi <rjustineduardo@yahoo.com>
2019-03-15 00:02:57 +0800

    added fade

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 //Let it rain!
 $(document).ready(function() {
+
   var canvas = $('#canvas')[0];
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -57,12 +58,36 @@ $(document).ready(function() {
     setInterval(draw, 40);
     
   }
+
+
+  /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1000);
+                    
+            }
+            
+        }); 
+    
+    });
+
 });
+
 
 //scroll bar hide shit
 // document.documentElement.style.overflowX = 'hidden';
 
-
+$("#mainNav").animate({'opacity':'1'},5000);
+$("#viewwork").animate({'opacity':'1'},3000);
 	//resizing shit
        initialize();
        function initialize() {
